c5da31289e05686565b278290fc831092e994a95
liyong <hungrybirder@gmail.com>
2022-03-11 19:06:48 +0800

    Add test_pidstatus()

--- tests/test_xproc.py
+++ tests/test_xproc.py
@@ -1,2 +1,10 @@
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-function-docstring
+
+from xproc import pidstatus
 
 
+def test_pidstatus():
+    pid1 = pidstatus.PIDStatus(1)
+    assert pid1
+    assert pid1.get(pidstatus.PS_PPID) == 0
